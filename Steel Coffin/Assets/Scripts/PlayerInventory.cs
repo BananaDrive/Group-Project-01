cb6a6a050fd983b6b269babe1c2175cb2475b70c
BananaDrive <Jmanfrohlich@gmail.com>
2024-11-19 13:12:21 -0800

    test

--- Steel Coffin/Assets/Scripts/PlayerInventory.cs
+++ Steel Coffin/Assets/Scripts/PlayerInventory.cs
@@ -16,12 +16,9 @@ public class PlayerInventory : MonoBehaviour
     {
         hotbarKeys = new KeyCode[9] { KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha5, KeyCode.Alpha6, KeyCode.Alpha7, KeyCode.Alpha8, KeyCode.Alpha9 };
 
-        for (int i = 0; i < inventory.Capacity; i++)
-        {
-            inventory.Add(null);
-        }
+        inventory = new List<GameObject>(new GameObject[9]);
 
-        pickupMessage.text = "";
+        //pickupMessage.text = "";
     }
 
     void Update()
