ae02f5ce3c84fdd57567d14d1ef5ddebd14ad754
Ripper4319 <filmguy4319@gmail.com>
2024-12-03 12:20:11 -0800

    distract

--- Steel Coffin/Assets/Scripts/Distractor script.cs
+++ Steel Coffin/Assets/Scripts/Distractor script.cs
@@ -1,22 +1,66 @@
+using System.Collections;
 using UnityEngine;
 
 public class Distractorscript : MonoBehaviour
 {
+    public float throwForce = .2f;
+    public float fuseTime = 3f;
+    public GameObject Distractprojectile;
+    public Transform player;
+    public Transform playerlookdirection;
+    public int currentdistract = 3;
+    public int maxdistract = 4;
+    public bool isthrowing = false;
+    public float throwrate = 2f;
+    public bool canthrow = true;
 
+    public bool distracttriggered = false;
 
-    public float Amount = 4;
-    public float DistractorVel = 4;
-    public bool isthrowing= false;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0) && currentdistract > 0 && !isthrowing)
+        {
+            distracttriggered = true;
+            ThrowGrenade();
+            isthrowing = true;
+        }
+
+
+        if (currentdistract <= 0)
+        {
+            Deactivateboomboom();
+        }
+
+    }
+
+    private void ThrowGrenade()
+    {
+
+        GameObject projectile = Instantiate(Distractprojectile, playerlookdirection.position, playerlookdirection.rotation * Quaternion.Euler(90, 0, 0));
+        Rigidbody rb = projectile.GetComponent<Rigidbody>();
+
+        if (rb != null)
+        {
+            rb.AddForce(playerlookdirection.transform.forward * throwForce, ForceMode.Impulse);
+        }
+        else
+        {
+
+        }
+
+        currentdistract--;
+
+        StartCoroutine(CooldownThrow());
+    }
+
+    private IEnumerator CooldownThrow()
+    {
+        yield return new WaitForSeconds(throwrate);
+        isthrowing = false;
+    }
+
+    public void Deactivateboomboom()
+    {
+        gameObject.SetActive(false);
     }
 }
