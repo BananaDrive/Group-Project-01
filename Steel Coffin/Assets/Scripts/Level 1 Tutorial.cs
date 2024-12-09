d6c7754462ee1b9450afbeb5472e38d3236f4b99
Ripper4319 <filmguy4319@gmail.com>
2024-12-09 12:59:50 -0800

    Tutorial hee hee

---
+++ Steel Coffin/Assets/Scripts/Level 1 Tutorial.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using TMPro;
+public class Level1Tutorial : MonoBehaviour
+{
+    public TextMeshProUGUI TutText;
+    public GameObject tutText;
+    public float Tut = 5;
+
+    void Start()
+    {
+        Tut = 0;
+        tutText.SetActive(true);
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Tut++;
+        }
+
+        if (Tut == 0)
+        {
+            TutText.text = "Press E for next Tip";
+        }
+
+        if (Tut == 1)
+        {
+            TutText.text = "WASD to walk, Shift to sprint!";
+        }
+
+        if (Tut == 2)
+        {
+            TutText.text = "throw objects with your mouse, the buttons corralate to direction!";
+        }
+
+        if (Tut == 3)
+        {
+            TutText.text = "there is a shack to your right!";
+        }
+
+        if (Tut == 4)
+        {
+            TutText.text = "press E to hide in the dumpster (red cube)";
+        }
+
+        if (Tut == 5)
+        {
+            TutText.text ="This is the end of the tutorial, oh look, a abandon hospital to your right";
+        }
+
+        if (Tut == 6)
+        {
+            TutText.text = "";
+        }
+    }
+
+
+}
